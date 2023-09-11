fac93cbf813c5796811d1f27c2cb63aeabcc54e9
MIke Ulm <maulmbuckeye@gmail.com>
2023-09-11 09:22:06 -0400

    Gets and plots Facebook data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from os.path import isfile
 
 from PIL import Image
 from matplotlib.patches import Patch, Circle
@@ -29,7 +30,7 @@ def get_us_geo_data(path_to_data: str):
     return geodata
 
 
-def translate_geometries(df, x, y, scale, rotate):
+def translate_geometries(df, x, y, scale, rotate):        # noqa
     df.loc[:, "geometry"] = df.geometry.translate(yoff=y, xoff=x)
     center = df.dissolve().centroid.iloc[0]
     df.loc[:, "geometry"] = df.geometry.scale(xfact=scale, yfact=scale, origin=center)
@@ -38,13 +39,23 @@ def translate_geometries(df, x, y, scale, rotate):
     return df
 
 
-def move_a_state(df, state_to_move: str, new_x, new_y, scale, rotate):
+def move_a_state(df, state_to_move: str, new_x, new_y, scale, rotate): # noqa
     df_state_to_move = df[df.STATEFP == state_to_move]
     df_other_states = df[~df.STATEFP.isin([state_to_move])]
 
     df_state_to_move = translate_geometries(df_state_to_move, new_x, new_y, scale, rotate)
 
     return pd.concat([df_other_states, df_state_to_move])
+
+
+def create_color(county_df: pd.DataFrame, data_breaks: list[tuple]) -> list[str]:
+    colors = []
+    for _, row in county_df.iterrows():
+        for p, c, _ in data_breaks:
+            if row.value >= np.percentile(county_df.value, p):
+                colors.append(c)
+                break
+    return colors
 
 
 def main():
@@ -62,11 +73,55 @@ def main():
 
     states = get_us_geo_data("./data/cb_2018_us_state_500k")
 
-    ax = counties.plot(edgecolor=edge_color + "55", color="None", figsize=(20, 20))
+    facebook_df = FacebookData()
+    facebook_df.get()
+    print(facebook_df.df.head(5))
+
+    county_id = "06075"
+
+    county_facebook_df = facebook_df.df[facebook_df.df.user_loc == county_id]
+
+    selected_color = "#fa26a0"
+    data_breaks = [
+        (90, "#00ffff", "Top 10%"),
+        (70, "#00b5ff", "90-70%"),
+        (50, "#6784ff", "70-50%"),
+        (30, "#aeb3fe", "50-30%"),
+        (0, "#e6e5fc", "Bottom 30%")
+    ]
+
+    counties.loc[:, "value"] = county_facebook_df.set_index("fr_loc").scaled_sci
+    counties.loc[:, "value"] = counties["value"].fillna(0)
+    counties.loc[:, "color"] = create_color(counties, data_breaks)
+    counties.loc[county_id, "color"] = selected_color
+
+    county_name = counties.loc[county_id].NAME
+
+    ax = counties.plot(edgecolor=edge_color + "55", color=counties.color, figsize=(20, 20))
     states.plot(ax=ax, edgecolor=edge_color, color="None", linewidth=1)
 
     plt.axis("off")
     plt.show()
+
+
+class FacebookData():
+    FACEBOOK_DATA = "./data/county_county.gzip"
+
+    def __init__(self):
+        pass
+
+    def get(self):
+        if isfile(self.FACEBOOK_DATA):
+            self.df = pd.read_parquet(self.FACEBOOK_DATA)  # noqa
+        else:
+            self._get_from_tsv()
+
+    def _get_from_tsv(self):
+        df = pd.read_csv("./data/county_county.tsv", sep="\t")     # noqa
+        df.user_loc = ("0" + df.user_loc.astype(str)).str[-5:]
+        df.fr_loc = ("0" + df.fr_loc.astype(str)).str[-5:]
+        df.to_parquet(self.FACEBOOK_DATA, compression="gzip")
+        self.df = df
 
 
 if __name__ == '__main__':
