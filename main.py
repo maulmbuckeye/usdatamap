9b639a0ad9b30cc804a3686ad3e140f68407d4a7
MIke Ulm <maulmbuckeye@gmail.com>
2023-09-11 09:33:50 -0400

    Add OH and CA FIPS code.
    This docments some research

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from matplotlib.patches import Patch, Circle
 # FIPS codes per state https://www.census.gov/library/reference/code-lists/ansi/ansi-codes-for-states.html
 UNINCORPORATED_TERRORIES = ["72", "69", "60", "66", "78"]
 ALASKA = "02"
+CALIFORNIA = "06"
 HAWAII = "15"
+OHIO = "39"
 
 
 def get_us_geo_data(path_to_data: str):
