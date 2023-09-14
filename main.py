3227977a6ff80acb808e235b941bd1c1f0175dba
MIke Ulm <maulmbuckeye@gmail.com>
2023-09-14 13:32:22 -0400

    Moved GeoConnections into its own file

--- main.py
+++ main.py
@@ -1,50 +1,4 @@
-import numpy as np
-
-import county as cty
-import facebook_connections as fbc
-import plot_counties as pc
-import usgeodata as usgd
-import usgeodatafactory as ugfac
-
-
-class GeoConnections:
-    def __init__(self, try_cache=True):
-        self.fac = ugfac.UsGeoDataFactory()
-        self.counties = None
-        self.states = None
-        self.facebook = None
-        self.get_data(try_cache)
-
-    def get_random_county(self):
-        return self.counties.get_random_fips()
-
-    def get_data(self, try_cache: bool = True):
-        self.states = self.fac.get("./data/cb_2018_us_state_500k", try_cache)
-        self.counties = self.fac.get("./data/cb_2018_us_county_500k", try_cache)
-        self.facebook = fbc.FacebookConnections()
-
-    def plot_a_county(self, candidate_county, p_data_breaks):
-        try:
-            the_county = cty.County(candidate_county, self.counties)
-        except usgd.IndexErrorRegionNotFound:
-            print(f"\t[[{candidate_county}]] is a not a valid FIPS")
-            return
-
-        self._update_county_color_using_fb_connections(the_county)
-        pc.plot_counties_by_connections_to_the_county(
-            the_county,
-            self.states,
-            self.counties,
-            p_data_breaks)
-
-    def _update_county_color_using_fb_connections(self, the_county: cty.County):
-        counties = self.counties
-        facebook = self.facebook
-        counties.assign_values(facebook.get_number_of_connections_from_county(the_county.fips))
-        counties.assign_colors(select_color_based_on_percentile(counties, data_breaks))
-        counties.assign_color_to_region(the_county.fips, pc.SELECTED_COLOR)
-
-        self.counties = counties
+from GeoConnect import GeoConnections
 
 
 def main():
@@ -63,50 +17,11 @@ def do_repl_loop(geo_connect):
             break
         elif response == "random":
             random_county = geo_connect.get_random_county()
-            geo_connect.plot_a_county(random_county, data_breaks)
+            geo_connect.plot_a_county(random_county)
         elif response == "refresh":
             geo_connect.get_data(try_cache=False)
         else:
-            geo_connect.plot_a_county(response, data_breaks)
-
-
-def assign_color_to_counties_by_facebook_connections(
-        counties: usgd.UsCountiesData,
-        facebook: fbc.FacebookConnections,
-        the_county: cty.County) -> usgd.UsGeoData:
-
-    counties.assign_values(facebook.get_number_of_connections_from_county(the_county.fips))
-    counties.assign_colors(select_color_based_on_percentile(counties, data_breaks))
-    counties.assign_color_to_region(the_county.fips, pc.SELECTED_COLOR)
-
-    return counties
-
-
-data_breaks = [
-    (90, pc.DISPLAY_GRADIENT_1, "Top 10%"),
-    (70, pc.DISPLAY_GRADIENT_2, "90-70%"),
-    (50, pc.DISPLAY_GRADIENT_3, "70-50%"),
-    (30, pc.DISPLAY_GRADIENT_4, "50-30%"),
-    (0, pc.DISPLAY_GRADIENT_5, "Bottom 30%")
-]
-
-
-def select_color_based_on_percentile(
-        counties: usgd.UsCountiesData,
-        p_data_breaks: list[tuple]) -> list[str]:
-
-    value_for_percentile = {
-        percentile: np.percentile(counties.value, percentile)
-        for percentile, _, _ in p_data_breaks
-    }
-
-    colors: list[str] = []
-    for _, county in counties.iter_all_counties():
-        for percentile, color, _ in p_data_breaks:
-            if county.value >= value_for_percentile[percentile]:
-                colors.append(color)
-                break
-    return colors
+            geo_connect.plot_a_county(response)
 
 
 if __name__ == '__main__':
