f0038fe351760424b3ddc4def2c0fac08980cef1
MIke Ulm <maulmbuckeye@gmail.com>
2023-09-14 19:12:37 -0400

    adds a nocache command line option

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import argparse
 
 
 def main():
-    is_using_random_selection = get_parameters()
-    geo_connect = GeoConnections(try_cache=True)
+    is_using_random_selection, use_cache = get_parameters()
+    geo_connect = GeoConnections(try_cache=use_cache)
     if is_using_random_selection:
         geo_connect.plot_a_random_county()
     else:
@@ -28,13 +28,16 @@ def do_repl_loop(geo_connect):
             geo_connect.plot_a_county(response)
 
 
-def get_parameters(argv=None) -> bool:
+def get_parameters(argv=None):
     parser = argparse.ArgumentParser(description='Create a Social Connectedness map of the US.')
-    parser.add_argument('-r','--random',
+    parser.add_argument('-r', '--random',
                         help='use a random county',
                         action='store_true')
+    parser.add_argument('-n', '--nocache',
+                        help='do not use cache, rebuild from local data',
+                        action='store_true')
     args = parser.parse_args(argv)
-    return args.random
+    return args.random, not args.nocache
 
 
 if __name__ == '__main__':
