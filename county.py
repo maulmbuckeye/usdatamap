deaca721b0046a9b92827bd49f54135d8d69252e
MIke Ulm <maulmbuckeye@gmail.com>
2023-09-14 08:57:18 -0400

    Uses __ to make variable private (not _ protected)

--- county.py
+++ county.py
@@ -3,18 +3,18 @@ import usgeodata as usgd
 
 class County:
     def __init__(self, fips: str, usgd_counties: usgd.UsGeoData):
-        self._fips = fips
-        self._name = usgd_counties.get_name_of(self._fips)
-        self._center_xy = usgd_counties.get_center_xy_of(self._fips)
+        self.__fips = fips
+        self.__name = usgd_counties.get_name_of(self.__fips)
+        self.__center_xy = usgd_counties.get_center_xy_of(self.__fips)
 
     @property
     def fips(self):
-        return self._fips
+        return self.__fips
 
     @property
     def name(self):
-        return self._name
+        return self.__name
 
     @property
     def center_xy(self):
-        return self._center_xy
+        return self.__center_xy
